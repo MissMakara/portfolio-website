29bb97ba584d67c84f703c8c284ab7d1229b7ef9
MissMakara <makaraisabel@gmail.com>
2024-02-09 20:12:07 +0300

    fix

--- email_api.py
+++ email_api.py
@@ -16,6 +16,15 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 load_dotenv()
 
+def print_exception():
+    exc_type, exc_obj, tb = sys.exc_info()
+    f = tb.tb_frame
+    lineno = tb.tb_lineno
+    filename = f.f_code.co_filename
+    linecache.checkcache(filename)
+    line = linecache.getline(filename, lineno, f.f_globals)
+    print('EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj))
+
 @app.route('/send_email', methods=['POST'])
 def send_email():
     try:
@@ -52,6 +61,7 @@ def send_email():
 
     except Exception as e:
         return ("An error occured: {}".format(str(e)))
+        print_exception()
 
 
 if __name__ == '__main__':
